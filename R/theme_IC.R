3c9eeccd076a2f8b90c45d7c12b4e29c8a526f1e
newton-c <newton.christop@gmail.com>
2023-05-17 16:40:31 -0300

    second test

--- R/theme_IC.R
+++ R/theme_IC.R
@@ -4,41 +4,49 @@
 # This theme requires Google fonts. You can download Google fonts here:
 # https://fonts.google.com/
 
-theme_IC <- function() {
-    font <- "Roboto"
+#' theme_ic
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+#'
+theme_ic <- function() {
+  font <- "Roboto"
 
-    ggplot2::theme(plot.title = ggplot2::element_text(family = "Roboto Black",
-                                                      size = 28),
-                   plot.subtitle = ggplot2::element_text(family = font,
-                       size = 22,
-                       color = "#3b3b3b",
-                       margin = ggplot2::margin(9, 0, 9, 0)),
-                   plot.caption = ggplot2::element_blank(),
-                   legend.position = "top",
-                   legend.text.align = 0,
-                   legend.background = ggplot2::element_blank(),
-                   legend.title = ggplot2::element_blank(),
-                   legend.key = ggplot2::element_blank(),
-                   legend.text = ggplot2::element_text(family = font,
-                                                       size = 18,
-                                                       color = "#3b3b3b"),
-                   axis.title = ggplot2::element_blank(),
-                   axis.text = ggplot2::element_text(family = font,
+  ggplot2::theme(plot.title = ggplot2::element_text(family = "Roboto Black",
+                                                    size = 28),
+                 plot.subtitle = ggplot2::element_text(family = font,
+                                                       size = 22,
+                                                       color = "#3b3b3b",
+                                                       margin = ggplot2::margin(9, 0, 9, 0)),
+                 plot.caption = ggplot2::element_blank(),
+                 legend.position = "top",
+                 legend.text.align = 0,
+                 legend.background = ggplot2::element_blank(),
+                 legend.title = ggplot2::element_blank(),
+                 legend.key = ggplot2::element_blank(),
+                 legend.text = ggplot2::element_text(family = font,
                                                      size = 18,
                                                      color = "#3b3b3b"),
-                   axis.text.x = ggplot2::element_text(
-                       margin = ggplot2::margin(5, b = 10),
-                       color = "#3b3b3b"),
-                   axis.ticks = ggplot2::element_blank(),
-                   axis.line = ggplot2::element_blank(),
-                   panel.grid.minor = ggplot2::element_blank(),
-                   panel.grid.major.x = ggplot2::element_blank(),
-                   panel.grid.major.y = ggplot2::element_line(
-                       color = "#3b3b3b"),
-                   panel.background = ggplot2::element_blank(),
-                   strip.background = ggplot2::element_rect(fill = "#FAFAFA"),
-                   strip.text = ggplot2::element_text(size = 22,
-                                                      hjust = 0,
-                                                      color = "#3b3b3b")
-    )
+                 axis.title = ggplot2::element_blank(),
+                 axis.text = ggplot2::element_text(family = font,
+                                                   size = 18,
+                                                   color = "#3b3b3b"),
+                 axis.text.x = ggplot2::element_text(
+                   margin = ggplot2::margin(5, b = 10),
+                   color = "#3b3b3b"),
+                 axis.ticks = ggplot2::element_blank(),
+                 axis.line = ggplot2::element_blank(),
+                 panel.grid.minor = ggplot2::element_blank(),
+                 panel.grid.major.x = ggplot2::element_blank(),
+                 panel.grid.major.y = ggplot2::element_line(
+                   color = "#3b3b3b"),
+                 panel.background = ggplot2::element_blank(),
+                 strip.background = ggplot2::element_rect(fill = "#FAFAFA"),
+                 strip.text = ggplot2::element_text(size = 22,
+                                                    hjust = 0,
+                                                    color = "#3b3b3b")
+  )
 }
--- README.md
+++ README.md
@@ -2,3 +2,25 @@
 
 to install:
 devtools::install_github("newton-c/ICplots", auth_token = "token_string")
+
+
+test:
+
+library(tidyverse)
+library(ICplots)
+
+x = 1:5
+y = 1:5 * 2
+
+dat <- as_tibble(cbind(x, y))
+
+ggplot(dat, aes(x = x, y = y)) +
+  geom_point() +
+  geom_line()
+  
+ggplot(dat, aes(x = x, y = y)) +
+  geom_point() +
+  geom_line() +
+  theme_ic()
+  
+  
