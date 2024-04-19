9499326e0710b144f21672f2a3821bb1f37aa9c5
newton-c <newton.christop@gmail.com>
2024-04-19 13:11:24 -0300

    Adding ic_logo function

---
+++ R/hline.R
@@ -0,0 +1,1 @@
+hline <- ggplot2::geom_hline(yintercept = 0, color = "#3B3B3B")
---
+++ R/ic_logo.R
@@ -0,0 +1,7 @@
+
+ic_logo <- function(x = 1, y = 1, hjust = 0, vjust = 0, size = 6) {
+  geom_text(aes(x = x, y = y,
+                label = "i\u200An\u200As\u200Ai\u200Ag\u200Ah\u200At\u200Ac\u200Ar\u200Ai\u200Am\u200Ae\u200A.\u200Ao\u200Ar\u200Ag"),
+            size=size, family = "Noto Serif", fontface = "italic",
+            colour = "#a5a5a5", hjust = hjust, vjust = vjust)
+}
